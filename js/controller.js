84a465c12ead3463d69a4d9b433003b8c442187a
Md Aqil <aqil@wipl.co.in>
2019-12-29 14:30:23 +0530

    added

--- js/controller.js
+++ js/controller.js
@@ -1,18 +1,24 @@
 var swiper = new Swiper('.swiper-container.swiper-testimonial', {
-    slidesPerView: 3,
+    slidesPerView: 1,
     spaceBetween: 30,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
+
     navigation: {
         nextEl: '.swiper-button-next-test',
         prevEl: '.swiper-button-prev-test',
     },
+
+    breakpoints: {
+        640: {
+            slidesPerView: 4,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+    }
 });
 var swiper = new Swiper('.swiper-container.swiper-work', {
-    slidesPerView: 5,
-    spaceBetween: 30,
+    slidesPerView: 2,
+    spaceBetween: 15,
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
@@ -21,6 +27,16 @@ var swiper = new Swiper('.swiper-container.swiper-work', {
         nextEl: '.swiper-button-next-work',
         prevEl: '.swiper-button-prev-work',
     },
+    breakpoints: {
+        640: {
+            slidesPerView: 5,
+            spaceBetween: 30,
+        },
+        768: {
+            slidesPerView: 5,
+            spaceBetween: 30,
+        },
+    }
 });
 
 $('.faq-heading').click(function () {
