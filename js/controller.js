22a519a1349c2b766cbdbfc1c2454e83d767a489
Md Aqil <aqil@wipl.co.in>
2020-01-05 01:57:33 +0530

    added'

--- js/controller.js
+++ js/controller.js
@@ -59,3 +59,19 @@ var swiper = new Swiper('.swiper-container.swiper-img-preveiw', {
 $('.faq-heading').click(function () {
     $(this).parent('li').find('.faq-text').slideToggle();
 });
+
+fetch("header.html")
+    .then(response => {
+        return response.text();
+    })
+    .then(data => {
+        document.querySelector("header").innerHTML = data;
+    });
+
+fetch("./footer.html")
+    .then(response => {
+        return response.text();
+    })
+    .then(data => {
+        document.querySelector("footer").innerHTML = data;
+    });
